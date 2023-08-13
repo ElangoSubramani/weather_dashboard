bc7b3c3163540bbdb8dc8ae385210f27fee086ce
ELANGO S <122277416+ElangoSubramani@users.noreply.github.com>
2023-08-13 11:04:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import plotly.express as px
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
                 meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
+server=app.server
 
 
 app.layout = dbc.Container(
